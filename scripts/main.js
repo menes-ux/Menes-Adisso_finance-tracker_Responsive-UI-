0b542eb91b284af23a7253aed93667cf05c81004
menes-ux <m.adisso@alustudent.com>
2025-10-16 11:55:26 +0200

    Adding animation

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,21 @@
 // We first have to allow the html to load before we can manipulate the DOM
 document.addEventListener('DOMContentLoaded', () => {
 
+  // We will add an event listenner to listen for burger click 
+   const hamburgerBtn = document.getElementById('hamburger-menu');
+    const mobileOverlay = document.getElementById('mobile-overlay');
+    const body = document.body;
+
+    // Here, when you click the hamburger, we toggle a class on the body.
+    hamburgerBtn.addEventListener('click', () => {
+        body.classList.toggle('sidebar-mobile-open');
+    });
+
+    // Here, if you click the dark overlay, it will close the menu.
+    mobileOverlay.addEventListener('click', () => {
+        body.classList.remove('sidebar-mobile-open');
+    });
+
     // After safely loading the DOM, we can now select the buttons and sections
     // To make it easier, we can store all buttons attributes in a list , same for the content sections
     const navButtons = document.querySelectorAll('.nav-button');
@@ -10,6 +25,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // I will go through each navigation button and add a click listener
   navButtons.forEach(button => {
     button.addEventListener('click', () => {
+
+      // This will close the sidebar when a link is clicked on mobile
+      if (window.innerWidth <= 1080) {
+          body.classList.remove('sidebar-mobile-open');
+      }
     //   I identified the button that was clicked using 'this' keyword
       const targetId = button.dataset.section;
 
